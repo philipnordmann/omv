cd6a2d845654c0acf63927c5006ba319807a3957
philipnordmann <nordmann.philip@gmail.com>
2021-11-04 21:13:21 +0100

    added packages

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ fi
 sudo apt-get install python3 python3-venv python3-pip -y
 python3 -m venv ansible-venv
 ${PWD}/ansible-venv/bin/pip3 install --upgrade pip
-${PWD}/ansible-venv/bin/pip3 install ansible
+${PWD}/ansible-venv/bin/pip3 install ansible docker
 
 ${PWD}/ansible-venv/bin/ansible-playbook install.yml -e "git_repo_url=${GIT_URL}" -e "git_username=${GIT_USERNAME}" -e "git_token=${GIT_TOKEN}" -e "tinc_name=${TINC_NAME}" --ask-become-pass
