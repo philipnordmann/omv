fbd0f5758d821e5de4935b2329ca04b59c7c5255
philipnordmann <nordmann.philip@googlemail.com>
2021-11-04 20:27:51 +0100

    added docker and tinc

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-OVPN_TMP_DL_URL="${1}"
-OVPN_USERNAME="${2}"
-OVPN_PASSWORD="${3}"
+TINC_NAME="${1}"
+GIT_URL="${2}"
+GIT_USERNAME="${3}"
+GIT_TOKEN="${4}"
 
-if [[ "${1}" == "" ||  "${2}" == "" ||  "${3}" == "" ]]; then
+if [[ "${1}" == "" ||  "${2}" == "" ||  "${3}" == "" ||  "${4}" == "" ]]; then
     echo "not all arguments provided"
     exit 1
 fi
@@ -14,4 +15,4 @@ python3 -m venv ansible-venv
 ${PWD}/ansible-venv/bin/pip3 install --upgrade pip
 ${PWD}/ansible-venv/bin/pip3 install ansible
 
-${PWD}/ansible-venv/bin/ansible-playbook install.yml -e "ovpn_download_url=${OVPN_TMP_DL_URL}" -e "ovpn_username=${OVPN_USERNAME}" -e "ovpn_password=${OVPN_PASSWORD}" --ask-become-pass
+${PWD}/ansible-venv/bin/ansible-playbook install.yml -e "git_repo_url=${GIT_URL}" -e "git_username=${GIT_USERNAME}" -e "git_token=${GIT_TOKEN}" -e "tinc_name=${TINC_NAME}" --ask-become-pass
